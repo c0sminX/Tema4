a9e52a768d4f41ca927ba4411700d8f64ee7fb28
cosmin <isaccosmin98@gmail.com>
2019-10-31 17:45:37 +0200

    Commit2 in Branch2

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.TextBox1.Location = new System.Drawing.Point(22, 30);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(462, 20);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Random Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 138);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
