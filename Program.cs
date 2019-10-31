d9a00b6de133947f4ba17dc605f41ca3d126b8aa
cosmin <isaccosmin98@gmail.com>
2019-10-31 17:47:11 +0200

    modificat

--- Program.cs
+++ Program.cs
@@ -17,6 +17,12 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+
         }
+
+
     }
 }
